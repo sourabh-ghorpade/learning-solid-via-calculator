4eb6063b7a75ec3b9b4c57a06298c693215d2852
Sourabh Ghorpade <sourabh.ghorpade@gmail.com>
2021-07-02 11:41:16 +0530

    +Ajinkya | Add addition and multiplication capabilitiy.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    
+    public static void main(String[] args) throws IOException {
+	    Double numberOne = 2.0, numberTwo = 2.0;
+	    String inputOperand = "+";
+        Double result = (double) -1;
+        switch (inputOperand) {
+            case "+" : result = numberOne + numberTwo;
+            break;
+            case "*": result = numberOne * numberTwo;
+            break;
+        }
+	    String outputResult = "result = " + result;
+        outputToConsole(outputResult);
+        outputToFile(outputResult);
+    }
+
+    private static void outputToFile(String outputResult) throws IOException {
+        FileWriter myWriter = new FileWriter("output.txt");
+        myWriter.write(outputResult);
+        myWriter.close();
+    }
+
+    private static void outputToConsole(String outputResult) {
+        System.out.println(outputResult);
     }
 }
